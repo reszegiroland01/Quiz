09143f6337748acafd174a959ea1c2cea6fba22c
reszegiroland01 <reszegiroland01@gmail.com>
2024-10-19 15:54:23 +0200

    sync

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function setUser(){
         }
         localStorage.setItem("user",JSON.stringify(userData))
     
-        window.location.href="../mode/index.html"
+        window.location.href="../QUIZ/mode/index.html"
     }
     
 }
